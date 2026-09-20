1a057cf2450472b41999982661ac9894c611438f
Gitium <gitium@presslabs.com>
2026-09-20 09:19:38 +0000

    updated plugin `Gitium` version 1.2.4

--- wp-content/plugins/gitium/gitium.php
+++ wp-content/plugins/gitium/gitium.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.presslabs.com
  * Text Domain:     gitium
  * Domain Path:     /languages
- * Version:         1.2.3
+ * Version:         1.2.4
  * License:         GPL3
  *
  * Gitium provides automatic git version control and deployment for
